32ad08b27f43f9bc43e7be1c3f4dc4d956b39e15
Valentin Lica <licavalentinh@gmail.com>
2022-06-18 09:02:32 +0200

    small refactor

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ### [Example](https://www.youtube.com/channel/UCXt2aUt0pmneWULS_1Nwvyg)
 
 # ⚠️ Work In Progress
+
+### Todo
+
+- [ ] Create custom thumbnail
+- [ ] Store settings into .json to be used in backend
+- [ ] Refactor
--- src/components/UI/SetupScreen.tsx
+++ src/components/UI/SetupScreen.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 
-import { LoadingIcon, LogoIcon } from "@icon";
+import { LoadingIcon } from "@icon";
 
 import styles from "@styles/components/UI/setup_screen.module.scss";
 
 const SetupScreen: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.branding}>
-          <LogoIcon />
+      <LoadingIcon />
 
-          <h3 className={styles.name}>Reddit Video Creator</h3>
-        </div>
-
-        <LoadingIcon />
-
-        <h1 className={styles.title}>Setting up things please wait...</h1>
-      </div>
+      <h1 className={styles.title}>Setting up things please wait...</h1>
     </div>
   );
 };
